ec322c63e001c108661cb9b9f3fb8b3d74a14ea1
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-06-09 15:54:01 +0200

    To do : Get data from db and add to front end.

--- GeniusRap/routes/api.php
+++ GeniusRap/routes/api.php
@@ -18,7 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::get('tracks', function (Request $request) {
     return Track::all();
 });

bfece58e4e1f3c87ff8799b407a1d66d0820fe7b
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-07-17 23:36:29 +0200

    Clean code

--- GeniusRap/app/Artist.php
+++ GeniusRap/app/Artist.php
@@ -5,13 +5,3 @@ namespace App;
 use Spatie\Searchable\Searchable;
 use Spatie\Searchable\SearchResult;
 use Illuminate\Database\Eloquent\Model;
-
-class Artist extends Model implements Searchable
-{
-    public function getSearchResult(): SearchResult
-    {
-       $url = route('artists.show', $this->id);
-
-       return new SearchResult($this, $this->name, $url);
-    }
-}
